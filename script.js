c5fe5b7b83e0cb5a416fd8442f7d3508ab20ff1c
kurumon <96.cinnamon@gmail.com>
2025-08-21 17:20:20 -0500

    Added javascript methods to update the site's behavior
    - Added a method to toggle audio playback
    - When music is playing, the marquee will show the song's info
    - The icon for the music nav item will change to signal it can be turned off
    - Marquee will show a nav item's description on hover

---
+++ script.js
@@ -0,0 +1,71 @@
+/*
+ * toggleMusic():
+ * Toggle music playback, and call updateMusicInfo().
+ */
+var audio = document.getElementById("audio");
+
+function toggleMusic() {
+  if (audio.paused) {
+    audio.play();
+    updateMusicInfo();
+  } else {
+    audio.pause();
+    updateMusicInfo();
+  }
+}
+
+/*
+ * updateMusicInfo():
+ * If music is playing update marquee to show the song's name,
+ * and change the music icon on the navigation panel to the stop music icon.
+ * If it's paused revert back to the default text and music icon.
+ */
+var icon = document.getElementById("nav-item-music");
+var marqueeText = document.getElementById("marquee-text");
+
+function updateMusicInfo() {
+  if (audio.paused) {
+    icon.style.background = "url('images/music.png') no-repeat center center";
+    marqueeText.innerHTML = "ψ(｀∇´)ψ DEATH TO LINKEDIN ＼(｀0´)／";
+  } else {
+    icon.style.background = "url('images/music-stop.png') no-repeat center center";
+    marqueeText.innerHTML = "♪ Now playing: Ryu Umemoto - Remains ♪";
+  }
+}
+
+/*
+ * When the mouse is over the navigation panel's items,
+ * make marquee-nav-info visible and show the item's description.
+ */
+const elements = document.getElementsByClassName("nav-item");
+var marqueeNavInfo = document.getElementById("marquee-nav-info");
+
+Array.from(elements).forEach((element) => {
+  element.addEventListener("mouseover", () => {
+    switch (element.id) {
+      case "nav-item-home":
+        marqueeNavInfo.innerHTML = "Return home";
+        break;
+      case "nav-item-about":
+        marqueeNavInfo.innerHTML = "More about me";
+        break;
+      case "nav-item-links":
+        marqueeNavInfo.innerHTML = "Other places you can find me";
+        break;
+      case "nav-item-music":
+        marqueeNavInfo.innerHTML = "Play some music";
+        break;
+    }
+    marqueeNavInfo.style.visibility = "visible";
+  });
+});
+
+/*
+ * When the mouse leaves the navigation panel's items,
+ * hide marquee-nav-info.
+ */
+Array.from(elements).forEach((element) => {
+  element.addEventListener("mouseout", () => {
+    marqueeNavInfo.style.visibility = "hidden";
+  });
+});
